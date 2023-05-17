170d81fc8e61ac74fd3cb8413ad94d53da63298e
Rafiulsam <rafiulsam69@gmail.com>
2023-05-17 14:35:23 +0600

    modified meals

--- js/meals.js
+++ js/meals.js
@@ -1,9 +1,17 @@
+const defaultData = () => {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s')
+        .then(res => res.json())
+        .then(data => displayMeals(data.meals))
+}
+defaultData()
+
 const loadMeals = (searchText) => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
     fetch(url)
         .then(res => res.json())
         .then(data => displayMeals(data.meals))
 }
+
 
 const displayMeals = meals => {
     const mealsContainer = document.getElementById('meals-container')
@@ -17,8 +25,25 @@ const displayMeals = meals => {
             <img src="${meal.strMealThumb}" class="card-img-top" alt="">
             <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                to additional content. This content is a little bit longer.</p>
+            <p class="card-text">Ingredient:
+            <ul class="list-unstyled">
+              <li>${meal.strIngredient1}</li>
+              <li>${meal.strIngredient2}</li>
+              <li>${meal.strIngredient3}</li>
+              <li>${meal.strIngredient4}</li>
+              <li>${meal.strIngredient5}</li>
+              <li>${meal.strIngredient6}</li>
+              <li>${meal.strIngredient7}</li>
+              <li>${meal.strIngredient8}</li>
+              <li>${meal.strIngredient9}</li>
+              <li>${meal.strIngredient10}</li>
+            </ul>
+            </p>
+            
+                <a onclick= "loadMealsDetails(${meal.idMeal})" type="button" class="fw-semibold text-warning text-decoration-underline"                       data-bs-toggle="modal" data-bs-target="#mealsDetailsModal">
+                     Details
+                </a>
+                
         </div>`
         mealsContainer.appendChild(mealDiv)
     });
@@ -26,9 +51,26 @@ const displayMeals = meals => {
 
 const searchMeals = () => {
     const searchText = document.getElementById('search-text').value;
+    document.getElementById('search-text').value = '';
     loadMeals(searchText)
 
 
 }
 
-loadMeals('fish')
+const loadMealsDetails = idMeals => {
+    console.log(idMeals);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeals}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealsDetails(data.meals[0]))
+}
+
+const displayMealsDetails = meal => {
+    document.getElementById('mealsDetailsModalLabel').innerText = meal.strMeal
+    const modalBody = document.getElementById('mealsDetailsModalBody')
+    modalBody.innerHTML = `
+    <img class="img-fluid" src="${meal.strMealThumb}" >
+    <h5 class="text-center mt-3">Instructions</h5>
+    <p class ="mt-4">${meal.strInstructions}</p>
+    `
+}
